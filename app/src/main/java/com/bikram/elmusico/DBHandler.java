e27714cb57920d522dfaf48fca54dc5f8e5895f5
Bikram Ghosh <git015.bikram.ghosh@gnkhalsa.edu.in>
2022-05-15 16:21:19 +0530

    added insert in db

--- app/src/main/java/com/bikram/elmusico/DBHandler.java
+++ app/src/main/java/com/bikram/elmusico/DBHandler.java
@@ -1,6 +1,8 @@
 package com.bikram.elmusico;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -8,16 +10,46 @@ import androidx.annotation.Nullable;
 
 public class DBHandler extends SQLiteOpenHelper {
     public DBHandler(@Nullable Context context) {
-        super(context, "MusicDb",null, 1);
+        super(context, "MusicDb", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase DB) {
-        DB.execSQL("create Table Userdetails(name TEXT primary key, contact TEXT, dob TEXT)");
+        DB.execSQL("create Table MusicTable(name TEXT primary key, artist_name TEXT not null, url TEXT not null)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("drop Table if exists MusicTable");
     }
+
+    public Boolean insertuserdata(String name, String artist_name, String url) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("artist_name", artist_name);
+        contentValues.put("url", url);
+        long result = DB.insert("MusicTable", null, contentValues);
+        if (result == -1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+//    public Boolean deletedata(String name) {
+//        SQLiteDatabase DB = this.getWritableDatabase();
+//        Cursor cursor = DB.rawQuery("Select * from Userdetails Where name=?", new String[]{name});
+//        if (cursor.getCount() > 0) {
+//            long result = DB.delete("Userdetails", "name=?", new String[]{name});
+//            if (result == -1) {
+//                return false;
+//            } else {
+//                return true;
+//            }
+//        } else {
+//            return false;
+//        }
+//    }
+
 }
--- app/src/main/java/com/bikram/elmusico/dbactivity.java
+++ app/src/main/java/com/bikram/elmusico/dbactivity.java
@@ -6,14 +6,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class dbactivity extends AppCompatActivity {
-    Button sendnextpage;
+    Button sendnextpage,insert;
+    DBHandler dbHandler;
+    EditText name,artist_name,url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dbactivity);
         sendnextpage = findViewById(R.id.nextpage);
+        insert = findViewById(R.id.insert);
+        name = findViewById(R.id.name);
+        artist_name = findViewById(R.id.artist_name);
+        url = findViewById(R.id.url);
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Boolean chckinsert = dbHandler.insertuserdata(name.getText().toString(),artist_name.getText().toString(),url.getText().toString());
+                if(chckinsert){
+                    Toast.makeText(dbactivity.this, "Success", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(dbactivity.this, "Unsuccessful", Toast.LENGTH_SHORT).show();
+            }
+            }
+        });
         sendnextpage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
